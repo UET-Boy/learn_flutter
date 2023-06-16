d872811c43fa5465434f20e9b1bbb3b11af223a2
a-hamzah <Spellbreaker907@gmail.com>
2023-06-16 09:15:43 +0500

    search box addition

--- jun23/todo_app/lib/screens/home.dart
+++ jun23/todo_app/lib/screens/home.dart
@@ -12,7 +12,48 @@ class Home extends StatelessWidget {
     return Scaffold(
         backgroundColor: tdBGColor,
         appBar: _buildAppBar(),
-        body: const Center(child: Text("This is home screen")));
+        body: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+          child: Column(
+            children: [
+              searchBox(),
+              Expanded(
+                child: ListView(
+                  children: [
+                    Container(
+                      margin: const EdgeInsets.only(top: 50, bottom: 20),
+                      child: const Text(
+                        'All ToDos',
+                        style: TextStyle(
+                            fontSize: 30, fontWeight: FontWeight.w500),
+                      ),
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+        ));
+  }
+
+  Widget searchBox() {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 15),
+      decoration: BoxDecoration(
+          color: Colors.white, borderRadius: BorderRadius.circular(20)),
+      child: const TextField(
+        decoration: InputDecoration(
+            contentPadding: EdgeInsets.all(0),
+            prefixIcon: Icon(
+              Icons.search,
+              color: tdBlack,
+            ),
+            prefixIconConstraints: BoxConstraints(maxHeight: 20, minWidth: 25),
+            border: InputBorder.none,
+            hintText: 'Search',
+            hintStyle: TextStyle(color: tdGrey)),
+      ),
+    );
   }
 
   AppBar _buildAppBar() {
